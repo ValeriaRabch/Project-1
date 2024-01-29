3dfe7b689cfde54a45c137b639ab81904eedb190
ValeriaRabch <rabchenukv@gmail.com>
2024-01-29 19:09:58 +0200

    Add func Print

--- Project1/Project1/FileName.cpp
+++ Project1/Project1/FileName.cpp
@@ -7,8 +7,15 @@ void Fulling(int* p, int size) {
 	}
 }
 
+void Print(int* p, int size) {
+	for (int i = 0; i < size; i++) {
+		cout << p[i] << ' ';
+	}
+}
+
 int main() {
 	int size = 10;
 	int* p = new int[size];
 	Fulling(p, size);
+	Print(p, size);
 }
